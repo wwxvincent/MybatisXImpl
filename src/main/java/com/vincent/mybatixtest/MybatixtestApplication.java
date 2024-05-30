de644680a08fb910bb24b893932007300fca2e9c
wwxvincent <wwxvincent727@gmail.com>
2024-05-30 14:52:20 +0800

    添加swagger

--- src/main/java/com/vincent/mybatixtest/MybatixtestApplication.java
+++ src/main/java/com/vincent/mybatixtest/MybatixtestApplication.java
@@ -3,8 +3,11 @@ package com.vincent.mybatixtest;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
 @MapperScan(basePackages ="com.vincent.mybatixtest.mapper")
 public class MybatixtestApplication {
 
--- src/main/java/com/vincent/mybatixtest/controller/AddressController.java
+++ src/main/java/com/vincent/mybatixtest/controller/AddressController.java
@@ -4,13 +4,11 @@ package com.vincent.mybatixtest.controller;
 import com.vincent.mybatixtest.entity.Address;
 import com.vincent.mybatixtest.entity.vo.AddressVO;
 import com.vincent.mybatixtest.service.IAddressService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -21,14 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-05-30
  */
 @RestController
-@RequestMapping("/address")
+@RequestMapping("api/address")
+@Api(tags = "Address Management", description = "Operation for Address management")
 @RequiredArgsConstructor
 public class AddressController {
 
     private final IAddressService addressService;
 
+    @ApiOperation("通过ID获取地址")
     @GetMapping("/getById")
     public Address selectById(@Param("id") Integer id) {
             return addressService.getById(id);
+    }
+
+    @ApiOperation("新增地址")
+    @PostMapping("/add")
+    public boolean addAddress(@RequestBody Address address) {
+        return addressService.save(address);
+    }
+
+    @ApiOperation("通过ID删除地址")
+    @PostMapping("/deleteById")
+    public boolean deleteById(@Param("id") Integer id) {
+        return addressService.removeById(id);
     }
 }
